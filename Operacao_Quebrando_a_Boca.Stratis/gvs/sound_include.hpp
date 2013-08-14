5a457c5af2dfaab1a2d985a02e88365f0f856d3f
Groo <ribeiro1981@gmail.com>
2013-08-14 20:57:26 -0300

    - Adicionados Fogos.
    Signed-off-by: Groo <ribeiro1981@gmail.com>

--- Operacao_Quebrando_a_Boca.Stratis/gvs/sound_include.hpp
+++ Operacao_Quebrando_a_Boca.Stratis/gvs/sound_include.hpp
@@ -1,14 +1,4 @@
-class CfgSounds
-{
-	sounds[]={};
-	class NoSound 
-	{
-		name = "NoSound";
-		sound[] = {"gvs\Dummy.ogg", 0, 1};
-		titles[] = {};
-	};
 
-};
   // soundName[] = {"path to file", VOL, PITCH, probability of being played, min delay, average delay, maximum delay after the sound};
   //sound1[]={"sounds\Trans_SFX.ogg",0.1,1,0.5,0,0,0};
   //empty[] = {not used, not used, not used, not used, minimum pause, average pause, maximum pause};
--- Operacao_Quebrando_a_Boca.Stratis/mission/occupation.hpp
+++ Operacao_Quebrando_a_Boca.Stratis/mission/occupation.hpp
@@ -1,15 +1,17 @@
 // FIRST LINE OF DEFENSE
 // GIRNA
 //--------------------------------------------------------------//
-nul=["mkrURBAN2",500,"garrison"] execVM "scripts\GrooEOS.sqf";
-nul=["mkrURBAN3",500,"garrison"] execVM "scripts\GrooEOS.sqf";
-nul=["mkrURBAN4",500,"garrison"] execVM "scripts\GrooEOS.sqf";
-nul=["mkrURBAN5",500,"garrison"] execVM "scripts\GrooEOS.sqf";
-nul=["mkrURBAN8",500,"garrison"] execVM "scripts\GrooEOS.sqf";
-nul=["mkrURBAN11",500,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN2",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN3",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN4",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN5",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN6",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN8",350,"garrison"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrURBAN11",350,"garrison"] execVM "scripts\GrooEOS.sqf";
 
-nul=["mkrHillPatrol",500,"squad"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrHillPatrol1",500,"squad"] execVM "scripts\GrooEOS.sqf";
 nul=["mkrHillPatrol2",500,"squad"] execVM "scripts\GrooEOS.sqf";
+nul=["mkrHillPatrol3",500,"squad"] execVM "scripts\GrooEOS.sqf";
 
 nul=[] execVM "mission\sdKillOfficer.sqf";
 
